18dac24d532f9850e6f0eebe728e52c74357b326
vagrant <vagrant@homestead>
2023-05-07 15:20:49 +0000

    Update vite config on laravelRouting

--- modul_03/laravelRouting/vite.config.js
+++ modul_03/laravelRouting/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+          host: "192.168.56.56",
+        },
+        host: "192.168.56.56",
+        watch: {
+          usePolling: true,
+        },
+    },
     plugins: [
         laravel({
             input: [
