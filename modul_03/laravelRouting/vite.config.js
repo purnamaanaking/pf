8583ec45f665c1ef087ff0631ca97ae726f13725
vagrant <vagrant@homestead>
2023-05-08 03:55:28 +0000

    Update

--- modul_03/laravelRouting/vite.config.js
+++ modul_03/laravelRouting/vite.config.js
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    server: {
-        hmr: {
-          host: "192.168.56.56",
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return  {
+        server: {
+            hmr: {
+            host: env.HOMESTEAD_HOST,
+            },
+            host: env.HOMESTEAD_HOST,
+            watch: {
+            usePolling: true,
+            },
         },
-        host: "192.168.56.56",
-        watch: {
-          usePolling: true,
-        },
-    },
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+        plugins: [
+            laravel({
+                input: [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+    }
 });
