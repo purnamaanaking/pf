c1afd34fd0b1524a3955d50a60e537850a3b2771
vagrant <vagrant@homestead>
2023-05-08 03:57:19 +0000

    Update

--- modul_04/laravelControllerView/resources/views/welcome.blade.php
+++ modul_04/laravelControllerView/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Belajar Controller & View pada Laravel</title>
     @vite('resources/sass/app.scss')
 </head>
 <body>
