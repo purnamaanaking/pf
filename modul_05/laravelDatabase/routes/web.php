18b5be9ecd4d303f439a8dd7af3b55360e723d96
vagrant <vagrant@homestead>
2023-05-11 06:58:23 +0000

    Update

---
+++ modul_05/laravelDatabase/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::resource('employees', EmployeeController::class);
+
+Route::get('profile', ProfileController::class)->name('profile');
