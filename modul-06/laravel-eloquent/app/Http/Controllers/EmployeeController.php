13f27aeda537b8ef925d42944637ae626c2f2eec
vagrant <vagrant@homestead>
2023-05-19 01:48:59 +0000

    Update

--- modul-06/laravel-eloquent/app/Http/Controllers/EmployeeController.php
+++ modul-06/laravel-eloquent/app/Http/Controllers/EmployeeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Employee;
 use App\Models\Position;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class EmployeeController extends Controller
